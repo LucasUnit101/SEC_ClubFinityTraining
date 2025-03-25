59e9a4cd768014f67975d8f5425b58d296c5755d
Lucas Romero-Wyhnanskyj <lucasr.w101@gmail.com>
2025-03-25 10:34:03 -0400

    Chapter 7

--- Module_2/js/main.js
+++ Module_2/js/main.js
@@ -1,2 +1,28 @@
-const anyName = "Lucas"
-console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+
+let choices = ["rock", "paper", "scissors"];
+
+const playerOne = choices[Math.random(Math.floor() * 3) + 1]
+const computer = choices[Math.random(Math.floor() * 3) + 1]
+
+
+if (playerOne === computer) {
+  console.log("You Tied!")
+} else if (playerOne === "rock") {
+  if (computer === "paper") {
+    console.log("You Lose!")
+  } else {
+    console.log("You Win!")
+  }
+} else if (playerOne === "paper") {
+  if (computer === "scissors") {
+    console.log("You Lose!")
+  } else {
+    console.log("You Win!")
+  }
+} else {
+  if (computer === "rock") {
+    console.log("You Lose!")
+  } else {
+    console.log("You Win!")
+  }
+}
